6fa326c2ec37dddecf98f29d68755a38db833d81
johnlindquist <johnlindquist@gmail.com>
2016-06-29 20:35:45 -0600

    adding package.json

--- app/main.ts
+++ app/main.ts
@@ -2,30 +2,40 @@ import yo from 'yo-yo';
 import {observable, autorun} from "mobx";
 
 const appState = observable({
-    people:[{
-      name: "John",
-      age: 35
+    people: [{
+        name: "John",
+        age: 35,
+        done: false
     }]
 });
 
 
-const inputComp = state =>
-  yo`<input value="${state.name}" oninput=${ev => state.name = ev.target.value}>`
+const createPerson = ()=> ({
+    name: "Rando",
+    age: 20,
+    done: false
+})
 
-const buttonComp = state => 
-  yo`<button onclick=${ev => state.age++}>Age up!</button>`;
+const inputComp = state =>
+    yo`<input value="${state.name}" oninput=${ev => state.name = ev.target.value}>`
+
+const buttonComp = state =>
+    yo`<button onclick=${ev => state.age++}>Age up!</button>`;
 
 const personComp = person => yo`<div>
   ${inputComp(person)}
   ${buttonComp(person)}
-  ${person.name} is ${person.age}
+  <div
+    style="text-decoration: ${person.done ? 'line-through':'none'}"
+    onclick=${ev => person.done = !person.done}    
+    >${person.name} is ${person.age}</div>
 </div>`
 
 const peopleList = state =>
-  state.people.map(person => personComp(person))
+    state.people.map(person => personComp(person))
 
 const appComp = state => yo`<div>
-  <button onclick=${ev => state.people.push({name: "Rando", age:20})}>Add Person</button>
+  <button onclick=${ev => state.people.push(createPerson())}>Add Person</button>
   <button onclick=${ev => console.log(state.people)}>Log state to console</button>
   ${peopleList(state)}
 </div>`;
